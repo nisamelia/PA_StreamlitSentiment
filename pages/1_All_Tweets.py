42edaea456bc4ca6e990843a4df6a37a5929abf9
nisamelia <amelianisa16@gmail.com>
2025-03-19 11:45:17 +0700

    add dinpar + tweet map

--- pages/1_All_Tweets.py
+++ pages/1_All_Tweets.py
@@ -1,18 +1,90 @@
 import streamlit as st
 import pydeck as pdk
+import pandas as pd
+
+## DEFINE DATA
+dinpar_df = pd.read_csv(r"C:\PA_Streamlit\data\dtw_jumlah_dinpar.csv")
+crawled_df = pd.read_csv(r"C:\PA_Streamlit\data\crawl_all_SA.csv")
+
+# COLOR_BREWER_BLUE_SCALE = [
+#     [240, 249, 232],
+#     [204, 235, 197],
+#     [168, 221, 181],
+#     [123, 204, 196],
+#     [67, 162, 202],
+#     [8, 104, 172],
+# ]
+
+view = pdk.data_utils.compute_view(dinpar_df[["bujur", "lintang"]])
+view_zoom = 3
+
+def show_map_dinpar(dinpar_df):
+    map_type = st.selectbox("Pilih Tipe Peta", ["Heat Map", "Hexagon"], key="map_dinpar")
+    dinpar = None
+    if map_type == "Heat Map":
+        dinpar = pdk.Layer(
+            "HeatmapLayer",
+            data = dinpar_df,
+            # opacity = 0.9,
+            get_position = ["bujur", "lintang"],
+            threshold = 0.2,
+            get_weight = "total",
+            pickable = True,
+            )
+    elif map_type == "Hexagon":
+        dinpar = pdk.Layer(
+            "HexagonLayer",
+            data = dinpar_df,
+            get_position = ["bujur", "lintang"],
+            auto_highlight = True,
+            get_weight="total",
+            extruded = True,
+            coverage = 1,
+            # radius = 200,
+            elevation_range=[0, 1000],
+            elevation_scale = 20,
+            pickable = True,
+            )
+    return pdk.Deck(
+        layers=[dinpar],
+        map_style="mapbox://styles/mapbox/navigation-night-v1",
+        initial_view_state=view,
+    )
+
+def show_map_tweet(crawled_df):
+    map_type = st.selectbox("Pilih Tipe Peta", ["Heat Map", "Hexagon"], key="map_tweet")
+    tweet = None
+    if map_type == "Heat Map":
+        tweet = pdk.Layer(
+            "HeatmapLayer",
+            data = crawled_df,
+            # opacity = 0.9,
+            get_position = ["bujur", "lintang"],
+            threshold = 0.2,
+            # get_weight = "total",
+            pickable = True,
+            )
+    elif map_type == "Hexagon":
+        tweet = pdk.Layer(
+            "HexagonLayer",
+            data = crawled_df,
+            get_position = ["bujur", "lintang"],
+            auto_highlight = True,
+            # get_weight="total",
+            extruded = True,
+            coverage = 1,
+            # radius = 200,
+            elevation_range=[0, 1000],
+            elevation_scale = 20,
+            pickable = True,
+            )
+    return pdk.Deck(
+        layers=[tweet],
+        map_style="mapbox://styles/mapbox/navigation-night-v1",
+        initial_view_state=view,
+    ) 
 
 st.set_page_config(layout="wide")
-
-# Define map styles (or layers)
-left_map = pdk.Deck(
-    map_style="mapbox://styles/mapbox/satellite-streets-v11",
-    initial_view_state=pdk.ViewState(latitude=0, longitude=0, zoom=3),
-)
-
-right_map = pdk.Deck(
-    map_style="mapbox://styles/mapbox/outdoors-v11",
-    initial_view_state=pdk.ViewState(latitude=0, longitude=0, zoom=3),
-)
 
 st.write("# PERBANDINGAN DATA DINAS PARIWISATA DAN DATA TWEETS WISATA")
 # Create two columns for the split map
@@ -20,8 +92,8 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.write("### Dinas Pariwisata DIY 2023")
-    st.pydeck_chart(left_map)
+    st.pydeck_chart(show_map_dinpar(dinpar_df))
 
 with col2:
     st.write("### Data Tweets Wisata DIY 2023")
-    st.pydeck_chart(right_map)
+    st.pydeck_chart(show_map_tweet(crawled_df))
--- pages/2_Sentiment_Tweets.py
+++ pages/2_Sentiment_Tweets.py
@@ -1,1 +1,10 @@
 import streamlit as st
+import pydeck as pdk
+import pandas as pd
+
+tweet_df = pd.read_csv(r"C:\PA_Streamlit\data\crawl_all_SA.csv")
+
+view = pdk.data_utils.computer_view(tweet_df["bujur", "lintang"])
+
+def show_map_sentimen(tweet_df):
+    map_type = st.selectbox("Pilih ")
